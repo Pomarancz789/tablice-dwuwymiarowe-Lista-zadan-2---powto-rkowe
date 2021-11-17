24a69f7df228fe193c227fbeb0be585cbdb255e2
Pomarancz789 <granat-02krawcy@icloud.com>
2021-11-17 09:23:02 +0100

    Initial Commit

---
+++ tablice dwuwymiarowe Lista zadań 2 - powtórkowe/main.cpp
@@ -0,0 +1,14 @@
+//
+//  main.cpp
+//  tablice dwuwymiarowe Lista zadań 2 - powtórkowe
+//
+//  Created by Jakub Wtorkiewicz on 17/11/2021.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
