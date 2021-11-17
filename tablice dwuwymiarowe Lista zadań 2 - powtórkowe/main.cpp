8bf22d68c1a256fa275e8dbd890ed13a8a8098e6
Pomarancz789 <granat-02krawcy@icloud.com>
2021-11-17 09:47:37 +0100

    wywalanie wiersza, przerobiłem wypisywanie na funkcje

--- tablice dwuwymiarowe Lista zadań 2 - powtórkowe/main.cpp
+++ tablice dwuwymiarowe Lista zadań 2 - powtórkowe/main.cpp
@@ -1,25 +1,52 @@
 #include <iostream>
 using namespace std;
 
-void zadaniepierwsze()
+void wypisz(int tab[3][5])
 {
+    for (int i=0; i<3; i++) {
+        for(int j=0; j<5; j++)
+        {
+            cout << tab[i][j] << " ";
+        }
+        cout << endl;
+    }
+    cout << "---------" <<endl;
+}
+
+void wywalwiersze(int tab[3][5], int wiersz)
+{
+    if(wiersz==2)
+    {
+        for(int i=0; i<5; i++)
+        {
+            tab[wiersz][i]=0;
+        }
+        wypisz(tab);
+    }
+    else
+    {
+        for(int i=0; i<5; i++)
+        {
+            tab[wiersz][i]=tab[wiersz+1][i];
+        }
+        wywalwiersze(tab, wiersz+1);
+    }
+}
+
+
+
+int main() {
+    //zadanie pierwsze
     int tab[3][5]={0};
     for (int i=0; i<3; i++) {
         for(int j=0; j<5; j++)
         {
             tab[i][j]=i+j;
-            cout << tab[i][j] << " ";
         }
-        cout << endl;
     }
-}
-
-
-int main() {
+    //zadanie pierwsze
     
-    
-    zadaniepierwsze();
-    
-    
+    wypisz(tab);
+    wywalwiersze(tab, 1);
     return 0;
 }
